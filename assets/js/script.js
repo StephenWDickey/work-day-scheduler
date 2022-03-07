e7292a890725b6620861e869fe35737c785c6bcc
stevey d <stphndcky22@gmail.com>
2022-03-06 16:02:11 -0800

    reformatted span elements to text input elements, added custom styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,8 @@ var createDiv = function() {
     for (var i=9; i<18; i++) {
         // displays hour
         currentHour = moment().format('HH')
-        // creates span element
-        var spanEl = document.createElement("span");
+        // creates input element
+        var input = document.createElement("input");
         // id has 9 added to it
         var innerDiv = $("#" + i);
         // creates division
@@ -57,28 +57,28 @@ var createDiv = function() {
         timeDiv.id += i;
         // adds text content to time div, displays hour and month
         timeDiv.textContent = moment(i, ["HH.mm"]).format("h A");
-        // applies data-* attributes to span element
-        spanEl.setAttribute("data-span-time-id", (i-9).toString());
+        // applies data-* attributes to input element
+        input.setAttribute("data-input-time-id", (i-9).toString());
 
         // if the current hour is greater than the index
         if (currentHour > i) {
             // label it as in the past
-            spanEl.className = "col-10 past";
+            input.className = "col-10 past";
         }
         // if the current index is equal to current hour then
-        else if (currentHour === i) {
+        else if (currentHour == i) {
             // label it as present
-            spanEl.className = "col-10 present";
+            input.className = "col-10 present";
         }
         // if the current time index is not in the present or past
         else {
-            spanEl.className = "col-10 future";
+            input.className = "col-10 future";
 
         }
 
-        // we will further specify className of span element
+        // we will further specify className of input element
         // this will designate tasks as past, present or future tasks
-        spanEl.className = spanEl.className + " task";
+        input.className = input.className + " task";
 
         // creates a division for saving task
         var saveDiv = document.createElement("div");
@@ -94,11 +94,12 @@ var createDiv = function() {
         // gives icon a data-* attribute
         icon.setAttribute("data-time-id", (i-9).toString());
 
+
         try {
-            spanEl.innerText = savedTasks.find( ({ timeID }) => timeID === (i-9).toString()).task;
+            input.innerText = savedTasks.find( ({ timeID }) => timeID === (i-9).toString()).task;
         }
         catch {
-            spanEl.innerText = "";
+            input.innerText = "";
         }
 
 
@@ -108,8 +109,8 @@ var createDiv = function() {
         saveDiv.append(icon);
         // attaches time division to container
         innerDiv.append(timeDiv);
-        // attaches span element to container
-        innerDiv.append(spanEl);
+        // attaches input element to container
+        innerDiv.append(input);
         // attaches the save button to container
         innerDiv.append(saveDiv);
 
@@ -121,10 +122,10 @@ var createDiv = function() {
 
 // add event listener to taskContainer element, the icon part
 // this will save our task in an array when we hit save button
-$("taskContainer").on("click", "i", function() {
+$(".fa-save").on("click", "i", function() {
     // retrieves value of attribute
     var timeID = $(this).attr("data-time-id");
-    var text = $("[data-span-time-id=${timeID}]").text();
+    var text = $("[data-input-time-id=${timeID}]").text();
     // we pass the timeID and text through the savedTasks array
     savedTasks(timeID, text);
 });
@@ -132,19 +133,19 @@ $("taskContainer").on("click", "i", function() {
 ////////////////////////////////////////////////////////////
 
 // event listener for taskContainer element
-// this will allow us to change span element text
-$("taskContainer").on("click", "span", function() {
+// this will allow us to change input element text
+$(taskContainer).on("click", "input", function() {
+
+    
     // make sure we trim any spaces from text entry
     var text = $(this).text().trim();
-    // sets attribute of data-span-time-id to timeID
-    var timeID = $(this).attr("data-span-time-id", timeID)
+    // sets attribute of data-input-time-id to timeID
+    var timeID = $(this).attr("data-input-time-id", timeID)
     // adds class to text area
     var textInput = $("<textarea>").addClass($(this).attr("class")).val(text);
-    textInput.attr("data-span-time-id", timeID);
+    textInput.attr("data-input-time-id", timeID);
 
-    $(this).replaceWith(textInput);
-    // when we 'focus' on text input it will be triggered
-    textInput.trigger("focus");
+    
 });
 
 /////////////////////////////////////////////////////////////////
@@ -153,17 +154,17 @@ $("taskContainer").on("click", "span", function() {
 $(".container").on("blur", "textarea", function() {
     // gets value of text area
     var text = $(this).val();
-    // retrieves value of attribute data-span-time-id
-    var timeID = $(this).attr('data-span-time-id');
+    // retrieves value of attribute data-input-time-id
+    var timeID = $(this).attr('data-input-time-id');
 
-    // recreates span element
-    var taskSpan = $("<span>").addClass($(this).attr("class")).text(text);
+    // recreates input element
+    var taskinput = $("<input>").addClass($(this).attr("class")).text(text);
     
-    // changes attribute of taskSpan element
-    taskSpan.attr("data-span-time-id", timeID)
+    // changes attribute of taskInput element
+    taskInput.attr("data-input-time-id", timeID)
 
     // replace text with new content
-    $(this).replaceWith(taskSpan);
+    $(this).replaceWith(taskInput);
 });
 
 //////////////////////////////////////////////////////
